ae0ede389dae412b02fff0c275946155455e0414
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-22 21:55:34 +0300

    displaying search categories

--- FoodRecipes/app/src/main/java/com/okanserdaroglu/foodrecipes/viewmodels/RecipeListViewModel.java
+++ FoodRecipes/app/src/main/java/com/okanserdaroglu/foodrecipes/viewmodels/RecipeListViewModel.java
@@ -3,18 +3,33 @@ package com.okanserdaroglu.foodrecipes.viewmodels;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 
 public class RecipeListViewModel extends AndroidViewModel {
 
     private static final String TAG = "RecipeListViewModel";
 
-    public RecipeListViewModel(@NonNull Application application) {
-        super(application);
+    public enum ViewState {CATEGORIES, RECIPES}
 
+    public LiveData<ViewState> getViewStateMutableLiveData() {
+        return viewStateMutableLiveData;
     }
 
+    private MutableLiveData<ViewState> viewStateMutableLiveData;
 
+    public RecipeListViewModel(@NonNull Application application) {
+        super(application);
+        init();
+    }
+
+    private void init() {
+        if (viewStateMutableLiveData == null) {
+            viewStateMutableLiveData = new MutableLiveData<>();
+            viewStateMutableLiveData.setValue(ViewState.CATEGORIES);
+        }
+    }
 }
 
 
