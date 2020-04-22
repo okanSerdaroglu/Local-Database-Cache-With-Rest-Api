b89fe2e8c3b8d27232482d18c3d22097391a62d6
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-23 01:10:18 +0300

    network bound resource p2

--- FoodRecipes/app/src/main/java/com/okanserdaroglu/foodrecipes/util/NetworkBoundResource.java
+++ FoodRecipes/app/src/main/java/com/okanserdaroglu/foodrecipes/util/NetworkBoundResource.java
@@ -1,12 +1,14 @@
 package com.okanserdaroglu.foodrecipes.util;
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
+import com.okanserdaroglu.foodrecipes.AppExecutors;
 import com.okanserdaroglu.foodrecipes.requests.responses.ApiResponse;
 
 
@@ -16,7 +18,44 @@ import com.okanserdaroglu.foodrecipes.requests.responses.ApiResponse;
 
 public abstract class NetworkBoundResource<CacheObject, RequestObject> {
 
-    private MutableLiveData<Resource<CacheObject>> results = new MutableLiveData<>();
+    private AppExecutors appExecutors;
+    private MediatorLiveData<Resource<CacheObject>> results = new MediatorLiveData<>();
+
+    public NetworkBoundResource(AppExecutors appExecutors) {
+        this.appExecutors = appExecutors;
+        init();
+    }
+
+    private void init (){
+        // update liveData for loading status
+        results.setValue((Resource<CacheObject>)Resource.loading(null));
+
+        // observe LiveData source from cache
+        final LiveData<CacheObject> dbSource = loadFromDb();
+
+        results.addSource(dbSource, new Observer<CacheObject>() {
+            @Override
+            public void onChanged(@Nullable CacheObject cacheObject) {
+                results.removeSource(dbSource);
+                if(shouldFetch(cacheObject)){
+                    // get data from network
+                } else {
+                    results.addSource(dbSource, new Observer<CacheObject>() {
+                        @Override
+                        public void onChanged(@Nullable CacheObject cacheObject) {
+                            setValue(Resource.success(cacheObject));
+                        }
+                    });
+                }
+            }
+        });
+    }
+    
+    private void setValue (Resource<CacheObject>newValue){
+        if (results.getValue() != newValue){
+            results.setValue(newValue);
+        }
+    }
 
     // Called to save the result of the API response into the database.
     @WorkerThread
