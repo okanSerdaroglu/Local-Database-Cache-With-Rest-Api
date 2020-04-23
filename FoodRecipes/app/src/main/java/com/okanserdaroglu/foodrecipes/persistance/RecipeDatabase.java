9cb0054f73d294457b8316201578b45ce0d1f59e
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-24 01:55:49 +0300

    type converters

--- FoodRecipes/app/src/main/java/com/okanserdaroglu/foodrecipes/persistance/RecipeDatabase.java
+++ FoodRecipes/app/src/main/java/com/okanserdaroglu/foodrecipes/persistance/RecipeDatabase.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import com.okanserdaroglu.foodrecipes.models.Recipe;
 
 @Database(entities ={Recipe.class},version = 1)
+@TypeConverters({Converters.class})
 public abstract class RecipeDatabase extends RoomDatabase {
 
     private static final String DATABASE_NAME = "recipes_db";
